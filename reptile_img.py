f3031dfd605be53605aa464b1d91141d753e2cdb
like-soup <like-soup@miao.com>
2019-12-07 21:35:47 +0800

    add reptile img demo

---
+++ reptile_img.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import shutil
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
+}
+
+url = 'https://movie.douban.com/chart'
+
+
+# 下载图片
+# Requests 库封装复杂的接口，提供更人性化的 HTTP 客户端，但不直接提供下载文件的函数。
+# 需要通过为请求设置特殊参数 stream 来实现。当 stream 设为 True 时，
+# 上述请求只下载HTTP响应头，并保持连接处于打开状态，
+# 直到访问 Response.content 属性时才开始下载响应主体内容
+
+
+def download_jpg(image_url, image_localpath):
+    response = requests.get(image_url, stream=True)
+    if response.status_code == 200:
+        with open(image_localpath, 'wb') as f:
+            response.raw.deconde_content = True
+            shutil.copyfileobj(response.raw, f)
+
+
+# 取得海报图片
+def craw3(url):
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    for pic_href in soup.find_all('a', class_='nbg'):
+        for pic in pic_href.find_all('img'):
+            imgurl = pic.get('src')
+            dir = os.path.abspath('.')
+            data_path = dir + "/data"
+            filename = os.path.basename(imgurl)
+            imgpath = os.path.join(data_path, filename)
+            print('开始下载 %s' % imgurl)
+            download_jpg(imgurl, imgpath)
+
+
+if __name__ == '__main__':
+    craw3(url)
+# craw3(url)
+#
+# 翻页
+# j = 0
+# for i in range(12, 37, 12):
+#     url = 'http://www.infoq.com/cn/presentations' + str(i)
+#     j += 1
+#     print('第 %d 页' % j)
+#     craw3(url)
